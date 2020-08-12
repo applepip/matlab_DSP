bf0c1b2a5fec79f74bbfb6c9ab7d0ef27c01e4e4
renliang <renliang@polarico.com>
2020-08-12 15:07:55 +0800

    程序更新

---
+++ 北京交通大学-陈后金-信号与系统matlab程序/M_filternoise_app.m
@@ -0,0 +1,23 @@
+% 利用M点的滑动平均系统滤除掉信号噪声
+
+% Siganl Smoothing by Moving Average Filter
+
+N = 201;
+n = 1.0*rand(1,N)-0.5; %噪声
+k = 0:N-1;
+
+s = 2*k.*(0.9.^k)+2.0*cos(0.02*pi*k) + 5.0; %滑动平均系统
+x= s+n;
+
+subplot(2,1,1);
+plot(k,n,'k-',k,s,'b--',k,x,'m-');
+xlabel('Time index k');
+legend('n[k]','s[k]','x[k]')
+
+M =5; b = ones(M,1)/M; a =[1];
+y = filter(b,a,x);
+
+subplot(2,1,2);
+plot(k,s,'b-', k,y,'r-');
+xlabel('Time index k'); 
+legend('s[k]','y[k]');
