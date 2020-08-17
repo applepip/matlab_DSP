e3cc7e86695f2b295683b3ca8caf77e2d0b77e89
renliang <renliang@polarico.com>
2020-08-17 19:05:59 +0800

    refresh

--- 数字信号处理实验教程-matlab仿真-唐向宏/disc_signal_wave.m
+++ 数字信号处理实验教程-matlab仿真-唐向宏/disc_signal_wave.m
@@ -50,8 +50,17 @@ x = [3,4,2,1,3,2];
 nx = [-2:3];
 [y,ny]=sigshift(x, nx, 3);
 figure(4);
-subplot(4,1,1);stem(nx,x);
-subplot(4,1,2);stem(ny,y);
+subplot(2,1,1);stem(nx,x);
+subplot(2,1,2);stem(ny,y);
+
+% 实现序列移位 y(n) = x(-n)
+x = [3,4,2,1,3,2];
+nx = [-2:3];
+[y,ny]=sigfold(x, nx);
+figure(5);
+grid on;subplot(2,1,1);stem(nx,x);axis([-2,6,0,4]);
+subplot(2,1,2);stem(ny,y);axis([-2,6,0,4]);
+
 
 function[y,n] = sigadd(x1, n1, x2, n2)
 % 求两个序列和y(n) = x1(n)+x2(n)
@@ -72,15 +81,24 @@ function[y,n] = sigmult(x1, n1, x2, n2)
     n = min(min(n1),min(n2)):max(max(n1),max(n2));
     y1=zeros(1,length(n));
     y2=y1; %利用zeros函数产生等长的两个序列y1(n),y2(n)
-    y1(find((n>=min(n1))&(n<=max(n1))==1)) = x1; %将x1(n)的值赋给y1(n)
+    y1(find((n>=min(n1))&(n<=max(n1))==1)) = x1; %将x1 (n)的值赋给y1(n)
     y2(find((n>=min(n2))&(n<=max(n2))==1)) = x2; %将x1(n)的值赋给y1(n)
     y = y1.*y2; %y1(n)与y2(n)对应点n相乘
 end
 
-function[y,n] = sigshift(x, nx, m)
+function[y,ny] = sigshift(x, nx, m)
 % 实现序列移位 y(n) = x(n-m)
 % x为输入的第一序列，n为序列x的时间序列
 % m为序列移位大小
     ny = nx+m;
     y=x;
+end
+
+
+function[y,ny] = sigfold(x, nx)
+% 实现序列翻转 y(n) = x(-n)
+% y为x(n)的翻转样本值
+% ny为y(n)的位置序列
+    y = fliplr(x);
+    ny = - fliplr(nx);
 end
